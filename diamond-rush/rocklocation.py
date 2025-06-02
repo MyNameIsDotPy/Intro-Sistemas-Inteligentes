d5e8c96a37f9d8e8dbf1bd8b952d1f4d0e4bf408
gabok2003 <gabriel.fonseca.guerrero@gmail.com>
2025-06-02 02:40:31 -0500

    Implementación transformada Hough coordenadas roca

---
+++ diamond-rush/rocklocation.py
@@ -0,0 +1,39 @@
+import cv2
+import numpy as np
+
+def detectar_piedras_redondas(imagen_path, mostrar_resultado=True):
+    # Cargar la imagen
+    imagen = cv2.imread(imagen_path)
+    gris = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
+    
+    # Aplicar desenfoque para reducir ruido
+    gris_suavizado = cv2.medianBlur(gris, 5)
+
+    # Detección de círculos con Hough
+    circulos = cv2.HoughCircles(gris_suavizado, 
+                                cv2.HOUGH_GRADIENT, 
+                                dp=1.2, 
+                                minDist=30,
+                                param1=50, 
+                                param2=30, 
+                                minRadius=10,
+                                maxRadius=17)
+
+    coordenadas = []
+    if circulos is not None:
+        circulos = np.uint16(np.around(circulos))
+        for (x, y, r) in circulos[0, :]:
+            coordenadas.append((x, y, r))
+            if mostrar_resultado:
+                cv2.circle(imagen, (x, y), r, (0, 255, 0), 2)
+                cv2.circle(imagen, (x, y), 2, (0, 0, 255), 3)
+
+    if mostrar_resultado:
+        cv2.imshow('Piedras Redondas Detectadas', imagen)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    return coordenadas
+
+coords = detectar_piedras_redondas("dmrush3.PNG")
+print("Coordenadas de piedras redondas:", coords)
